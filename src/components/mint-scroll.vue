eb25d10ff610f91a1cc4d30d0f36fcf5758fa7ca
gongph <gongph@foxmail.com>
2016-10-29 10:45:06 +0800

    add files src/*

---
+++ src/components/mint-scroll.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="mint-scroll" :style="'height:' + height"><slot></slot></div>
+</template>
+
+<script>
+	import scroll from '../units/scroll'
+
+	export default {
+		props: {
+			speed: {
+				type: String,
+				default: '20'
+			},
+			height: {
+				type: String,
+				default: '300px'
+			}
+		},
+		computed: {
+			numberSpeed () {
+				return parseInt(this.speed)
+			}
+		},
+		mounted () {
+			// 启动滚动
+			scroll('.mint-scroll', {
+				speed : this.numberSpeed,
+				height: this.height
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.mint-scroll {
+		overflow: hidden;
+	}
+</style>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './example/app'
+
+new Vue({
+	el: '#app',
+	render: cb => cb(App)
+});
